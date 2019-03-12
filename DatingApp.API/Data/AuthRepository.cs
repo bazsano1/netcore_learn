0a2699eefbd956e73c59d48318d0a145e0b8be9c
Balazs Toth <bazsano1@gmail.com>
2019-03-12 22:02:45 +0100

    added user classes and login workflow

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext context;
+
+        public AuthRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+
+        public async Task<User> Login(string username, string password)
+        {
+
+            var user = await context.Users.FirstOrDefaultAsync(_ => _.UserName == username);
+
+            if (user != null)
+            {
+                var hashedPassword = GetHashedPassword(password, user.PasswordSalt);
+
+
+                if (user.PasswordHash.SequenceEqual(hashedPassword))
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        private byte[] GetHashedPassword(string password, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                return hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
